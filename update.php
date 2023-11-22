e3aafa1345f8c1b5daf4cc5543fefc1ef497ac42
Alezuna Zikri <alezunazikri@Laptop-Ai.local>
2023-11-22 14:05:39 +0700

    first commit

---
+++ update.php
@@ -0,0 +1,82 @@
+<?php
+include"config.php";
+$id =$_GET['edit'];
+$query ="SELECT * FROM `tb_note` WHERE id_note='$id'";
+$sql =mysqli_query($conn,$query);
+$result = mysqli_fetch_assoc($sql);
+
+$no = 0;
+
+// var_dump($result);
+// die();
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Note-Ku</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <style>
+        body{
+    background-image: url(https://i.pinimg.com/474x/72/3a/e6/723ae6dbf882f37e3fd3d1696b1b8255.jpg);
+        }
+        h4{
+            color:white;
+            
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container mt-3">
+        <h4 >
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-journal-text" viewBox="0 1.4 16 16">
+  <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+  <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+  <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+        </svg>
+        EDIT Your-Note
+        </h4>
+        <div class="card shadow bg-body-tertiary rounded mb-4 mt-3">
+            <div class="card-body">
+                <h2 class="judul">Edit Here !!!</h2>
+
+                <form class="d-flex" action="updateupdate.php" method="post">
+                    <input class="form-control me-2" name="title_note" type="text" placeholder="TITLE..." value="<?php echo $result['title_note'] ?>">
+                    <input class="form-control me-2" name="note_note" type="text" placeholder="CONTENTS..." value="<?php echo $result['note_note'] ?>">
+                    <input type="hidden" name="id" value="<?php echo $result['id_note'] ?>">
+
+                    <button class="btn btn-primary" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+</svg>Edit
+                    </button>
+                </form>
+                
+                
+            </div>
+        </div>
+
+        
+
+
+
+                
+                   
+
+            </div>
+       
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
+
+</body>
+</html>
